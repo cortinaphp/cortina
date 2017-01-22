8442b1262416229e3271648b3dfc37355b5054a9
Ross Chater <rcapple@me.com>
2017-01-22 00:04:58 +0000

    Add initial files

---
+++ tests/TestCase/AppTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Cortina : PHP Framework
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Ross Chater. (http://rosschater.com)
+ * @link          http://rosschater.com Project Cortina
+ * @since         0.0.1
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+namespace App\Test\TestCase;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * App Test
+ */
+class AppTest extends TestCase
+{
+
+    
+
+}
