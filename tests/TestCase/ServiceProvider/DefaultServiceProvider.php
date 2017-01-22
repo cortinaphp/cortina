54aac1189cdaae326cabf648608781482f605ba6
Ross Chater <rcapple@me.com>
2017-01-22 16:58:10 +0000

    Add test file

---
+++ tests/TestCase/ServiceProvider/DefaultServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Cortina : PHP Framework
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Ross Chater. (http://rosschater.com)
+ * @link          http://rosschater.com Project Cortina
+ * @since         0.0.1
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Cortina\Test\TestCase\ServiceProvider;
+
+use Cortina\ServiceProvider\DefaultServiceProvider;
+
+/**
+ * Default Service Provider Test
+ */
+class DefaultServiceProviderTest extends TestCase
+{
+
+}
