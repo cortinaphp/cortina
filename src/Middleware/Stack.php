7eb6e531008eaedfe854afe537a05190b37d03b6
Ross Chater <rcapple@me.com>
2017-01-26 00:56:49 +0000

    Add stack with add, remove and get methods

--- src/Middleware/Stack.php
+++ src/Middleware/Stack.php
@@ -1,9 +1,5 @@
 <?php
-
 namespace Cortina\Middleware;
-
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 
 class Stack
 {
@@ -12,89 +8,38 @@ class Stack
      * Middlewares callable
      * @var array
      */
-    protected $middlewares = [];
+    private $middleware = [];
 
     /**
-     * Current Middleware Stack position
-     * @var integer
+     * Get middleware with index
+     * @param  integer $index
+     * @return callable|false
      */
-    protected $current = 0;
-
-    /**
-     * Return an instance with the specified middleware added to the stack.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the stack, and MUST return an instance that contains
-     * the specified middleware.
-     *
-     * @param MiddlewareInterface $middleware
-     *
-     * @return self
-     */
-    public function withMiddleware($middleware)
+    public function get($index)
     {
-        $stack = clone $this;
-        $key = array_search($middleware, $stack->middlewares);
+        if (isset($this->middleware[$index])) {
+            return $this->middleware[$index];
+        }
+        return false;
+    }
+
+
+    public function add($middleware)
+    {
+        $key = array_search($middleware, $this->middleware);
         if ($key === false) {
-            $stack->middlewares[] = $middleware;
+            $this->middleware[] = $middleware;
         }
-        return $stack;
+        return $this;
     }
 
-    /**
-     * Return an instance without the specified middleware.
-     *
-     * This method MUST be implemented in such a way as to retain the
-     * immutability of the stack, and MUST return an instance that does not
-     * contain the specified middleware.
-     *
-     * @param MiddlewareInterface $middleware
-     *
-     * @return self
-     */
-    public function withoutMiddleware($middleware)
+    public function remove($middleware)
     {
-        $stack = clone $this;
-        $key = array_search($middleware, $stack->middlewares);
+        $key = array_search($middleware, $this->middleware);
         if ($key !== false) {
-            unset($stack->middlewares[$key]);
+            unset($this->middleware[$key]);
         }
-        return $stack;
-    }
-
-    /**
-     * Process the request through middleware and return the response.
-     *
-     * This method MUST be implemented in such a way as to allow the same
-     * stack to be reused for processing multiple requests in sequence.
-     *
-     * @param RequestInterface $request
-     *
-     * @return ResponseInterface
-     */
-    public function process(RequestInterface $request, ResponseInterface $response)
-    {
-        return $this->__invoke($request, $response);
-    }
-
-    /**
-     * Invoke the middleware instances
-     * @param  RequestInterface  $request
-     * @param  ResponseInterface $response
-     * @return \Psr\Http\Message\ResponseInterface
-     */
-    public function __invoke(RequestInterface $request, ResponseInterface $response)
-    {
-        if (isset($this->middlewares[$this->current])) {
-            $middleware = $this->middlewares[$this->current];
-            if ($middleware) {
-
-                $this->current++;
-                return $middleware($request, $response, $stack);
-            }
-        }
-
-        return $response;
+        return $this;
     }
 
 }
